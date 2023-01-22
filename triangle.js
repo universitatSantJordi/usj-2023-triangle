5ca26c8d071e3e3f6936a379b4615b8aa2007e94
Daniel Coloma <danielcoloma@gmail.com>
2023-01-22 11:08:31 +0100

    Update triangle.js

--- triangle.js
+++ triangle.js
@@ -1,34 +1,37 @@
-INVALID_ARGUMENTS = "The arguments were not valid";
-NOT_A_TRIANGLE = "Impossible to create a triangle with those sides";
-EQUILATERAL = "Equilateral triangle";
-ISOSCELES = "Isosceles triangle";
-SCALENE = "Scalene triangle";
+var INVALID_ARGUMENTS = 'The arguments were not valid';
+var NOT_A_TRIANGLE = 'Impossible to create a triangle with those sides';
+var EQUILATERAL = 'Equilateral triangle';
+var ISOSCELES = 'Isosceles triangle';
+var SCALENE = 'Scalene triangle';
 
 /*
  * Given the three sides of a triangle it calculates the type of the
  * triangle.
  */
-function getTriangleType(a, b, c) {
-  type = "";
+function getTriangleType(lengthA, lengthB, lengthC) { // eslint-disable-line no-unused-vars
+  var type = '';
   // In Javascript isNaN returns true if the argument is Not a Number (NaN)
-  if ( isNaN(a) || isNaN(b) || isNaN(c) ) { //Inputs are not numbers
+  if ( isNaN(lengthA) || isNaN(lengthB) || isNaN(lengthC) ) { //Inputs are not numbers
     type = INVALID_ARGUMENTS;
   } else { // Inputs are numbers
     // Convert from objects to float
-    a = parseFloat(a);
-    b = parseFloat(b);
-    c = parseFloat(c);
+    var a = parseFloat(lengthA);
+    var b = parseFloat(lengthB);
+    var c = parseFloat(lengthC);
     if ((a <= 0) || (b <= 0) || (c <= 0)) { // A side is not positive
       type = INVALID_ARGUMENTS;
     } else if ( (a+b <= c) || (a+c <= b) || (b+c <= a)) { // invalid triangle
       type = NOT_A_TRIANGLE;
-    } else if ((a == b) && (b == c)) { // all sides equal = equilateral
-      type = EQUILATERAL;
-    } else if ((a == b) || (b == c) || (a == c)) { // 2 sides = isosceles
-      type = ISOSCELES;
     } else { // All sides different = scalene
-      type = SCALENE;
+      if ( (a==b) && (b==c)) {
+        type = EQUILATERAL;
+      } else if ( (a==b) || (b==c) || (a==c)) {
+        type = ISOSCELES;
+      } else {
+        type = SCALENE;
+      }
     }
   }
+  
   return type;
 }
