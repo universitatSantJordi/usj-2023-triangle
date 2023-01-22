562cc230ac404509ee1dc90aeb03675864ad71d2
Daniel Coloma <danielcoloma@gmail.com>
2023-01-22 10:58:18 +0100

    Add linting rules

---
+++ .estlinrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "sourceType": "module"
+    },
+    "rules": {
+        "no-cond-assign": [
+            "error",
+            "always"
+        ],
+        "indent": [
+            "error",
+            2
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "no-unused-vars": "off",
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
+};
